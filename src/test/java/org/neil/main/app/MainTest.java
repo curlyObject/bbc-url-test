95a3f3f17c6207f7df51c6b1e99af94583b75438
Neil Bateman <neil.bateman@tide.co>
2017-11-02 00:51:48 +0000

    Setup Mock Server and Results
    Configure the mock server and set it up per test case to return the expected outputs.

--- src/test/java/org/neil/main/app/MainTest.java
+++ src/test/java/org/neil/main/app/MainTest.java
@@ -1,20 +1,21 @@
 package org.neil.main.app;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.http.Fault;
 import com.github.tomakehurst.wiremock.http.HttpHeader;
 import com.github.tomakehurst.wiremock.http.HttpHeaders;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -26,126 +27,332 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MainTest {
 
     private static final String CONTENT_LENGTH = "Content-Length";
+    private static final long LENGTH = 12345L;
+    private static final String MOCK_SERVER = "localhost";
+    private static final String TEST_PATH = "/test";
     private final String lineEnding = System.lineSeparator();
 
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    // Sets up and tears down the mock server with random available ports per test case.
-    @Rule
-    public WireMockRule wireMockRule = new WireMockRule(wireMockConfig().dynamicPort().dynamicHttpsPort());
+
+    private WireMockServer wireMockServer;
 
     @Before
-    public void setUpStreams() {
+    public void setUp() {
 
         System.setOut(new PrintStream(outContent));
     }
 
     @After
-    public void cleanUpStreams() {
+    public void tearDown() {
 
+        if (Objects.nonNull(wireMockServer)) {
+            wireMockServer.stop();
+        }
         System.setOut(null);
     }
 
     @Test
     public void statusDocument_WhenUrlFound() {
 
-        ZonedDateTime expectedDate = ZonedDateTime.now();
+        startMockServer(MOCK_SERVER);
 
-        final HttpHeaders expectedHeaders = new HttpHeaders(buildContentLengthHeader(12345L),
+        final String expectedDate = getDate();
+        final String expectedUrl = buildExpectedHttpUrl(MOCK_SERVER, TEST_PATH);
+
+        final HttpHeaders expectedHeaders = new HttpHeaders(buildContentLengthHeader(LENGTH),
                 buildDateHeader(expectedDate));
 
-        stubFor(get(urlEqualTo("/my/resource"))
-                .willReturn(aResponse()
-                        .withStatus(200)
-                        .withHeaders(expectedHeaders)));
+        final int statusCode = 200;
+        stubUrl(TEST_PATH, expectedHeaders, statusCode);
 
-        Main.main("http://www.bbc.co.uk/");
+        Main.main(expectedUrl);
 
-        final String output = outContent.toString();
-        final String[] outputLines = output.split(lineEnding);
+        assertStatusDocument(expectedDate, expectedUrl, statusCode, LENGTH, extractStandardOutput());
 
-        assertThat(outputLines)
-                .hasSize(6)
-                .containsExactly(
-                        "{",
-                        "\"Url\": \"http://www.bbc.co.uk/iplayer\",",
-                        "\"Status_code\":200,",
-                        "\"Content_length\":209127,",
-                        "\"Date\":\"Tue, 25 Jul 2017 17:00:55 GMT\"",
-                        "}"
-                );
+    }
+
+    @Test
+    public void statusDocument_WhenUrlHttps() {
+
+        startMockServer(MOCK_SERVER);
+
+        final String expectedDate = getDate();
+        final String expectedUrl = buildExpectedHttpsUrl(MOCK_SERVER, TEST_PATH);
+
+        final HttpHeaders expectedHeaders = new HttpHeaders(buildContentLengthHeader(LENGTH),
+                buildDateHeader(expectedDate));
+
+        final int statusCode = 200;
+        stubUrl(TEST_PATH, expectedHeaders, statusCode);
+
+        Main.main(expectedUrl);
+
+        assertStatusDocument(expectedDate, expectedUrl, statusCode, LENGTH, extractStandardOutput());
 
     }
 
     @Test
     public void statusDocument_WhenUrlRedirects() {
 
-        Main.main("https://google.com");
+        startMockServer(MOCK_SERVER);
+
+        final String expectedDate = getDate();
+        final String expectedUrl = buildExpectedHttpUrl(MOCK_SERVER, TEST_PATH);
+
+        final HttpHeaders expectedHeaders = new HttpHeaders(buildContentLengthHeader(LENGTH),
+                buildDateHeader(expectedDate));
+
+        final int statusCode = 308;
+        stubUrl(TEST_PATH, expectedHeaders, statusCode);
+
+        Main.main(expectedUrl);
+
+        assertStatusDocument(expectedDate, expectedUrl, statusCode, LENGTH, extractStandardOutput());
+
     }
 
     @Test
     public void statusDocument_WhenUrlModePermanently() {
 
-        Main.main("http://bbc.co.uk/");
+        startMockServer(MOCK_SERVER);
+
+        final String expectedDate = getDate();
+        final String expectedUrl = buildExpectedHttpUrl(MOCK_SERVER, TEST_PATH);
+
+        final HttpHeaders expectedHeaders = new HttpHeaders(buildContentLengthHeader(LENGTH),
+                buildDateHeader(expectedDate));
+
+        final int statusCode = 301;
+        stubUrl(TEST_PATH, expectedHeaders, statusCode);
+
+        Main.main(expectedUrl);
+
+        assertStatusDocument(expectedDate, expectedUrl, statusCode, LENGTH, extractStandardOutput());
+
     }
 
     @Test
     public void statusDocument_WhenUrlNotModified() {
 
-        Main.main("http://bbc.co.uk/kitten/pics.jpg");
-    }
+        startMockServer(MOCK_SERVER);
 
-    @Test
-    public void statusDocument_WhenUrlDoesNotResolve() {
+        final String expectedDate = getDate();
+        final String expectedUrl = buildExpectedHttpUrl(MOCK_SERVER, TEST_PATH);
 
-        Main.main("http://not.exists.bbc.co.uk/");
+        final HttpHeaders expectedHeaders = new HttpHeaders(buildContentLengthHeader(LENGTH),
+                buildDateHeader(expectedDate));
+
+        final int statusCode = 304;
+        stubUrl(TEST_PATH, expectedHeaders, statusCode);
+
+        Main.main(expectedUrl);
+
+        assertStatusDocument(expectedDate, expectedUrl, statusCode, LENGTH, extractStandardOutput());
+
     }
 
     @Test
     public void statusDocument_WhenUrlPageNotFound() {
 
-        Main.main("http://www.bbc.co.uk/missing/thing");
+        startMockServer(MOCK_SERVER);
+
+        final String expectedDate = getDate();
+        final String expectedUrl = buildExpectedHttpUrl(MOCK_SERVER, TEST_PATH);
+
+        final HttpHeaders expectedHeaders = new HttpHeaders(buildContentLengthHeader(LENGTH),
+                buildDateHeader(expectedDate));
+
+        final int statusCode = 404;
+        stubUrl(TEST_PATH, expectedHeaders, statusCode);
+
+        Main.main(expectedUrl);
+
+        assertStatusDocument(expectedDate, expectedUrl, statusCode, LENGTH, extractStandardOutput());
     }
 
     @Test
-    public void errorReport_WhenProtocolNotSupported() {
+    public void statusDocument_WhenContentLengthMissing() {
 
-        Main.main("ftp://who/");
+        startMockServer(MOCK_SERVER);
+
+        final String expectedDate = getDate();
+        final String expectedUrl = buildExpectedHttpUrl(MOCK_SERVER, TEST_PATH);
+
+        final HttpHeaders expectedHeaders = new HttpHeaders(buildDateHeader(expectedDate));
+
+        final int statusCode = 200;
+        stubUrl(TEST_PATH, expectedHeaders, statusCode);
+
+        Main.main(expectedUrl);
+
+        assertStatusDocument(expectedDate, expectedUrl, statusCode, null, extractStandardOutput());
     }
 
     @Test
-    public void errorReport_WhenUrlInvalid() {
+    public void statusDocument_WhenDateMissing() {
 
-        Main.main("foo/bar");
+        startMockServer(MOCK_SERVER);
+
+        final String expectedDate = null;
+        final String expectedUrl = buildExpectedHttpUrl(MOCK_SERVER, TEST_PATH);
+
+        final HttpHeaders expectedHeaders = new HttpHeaders(buildContentLengthHeader(LENGTH));
+
+        final int statusCode = 200;
+        stubUrl(TEST_PATH, expectedHeaders, statusCode);
+
+        Main.main(expectedUrl);
+
+        assertStatusDocument(expectedDate, expectedUrl, statusCode, LENGTH, extractStandardOutput());
     }
 
     @Test
-    public void errorReport_WhenResponseMalformed() {
+    public void errorDocument_WhenUrlDoesNotResolve() {
 
-        Main.main("foo/bar");
+        startMockServer(MOCK_SERVER);
+
+        final String expectedDate = getDate();
+        final String expectedUrl = buildExpectedHttpUrl(MOCK_SERVER, "/fake");
+        final String expectedError = "URL could not be resolved by the DNS server";
+
+        final HttpHeaders expectedHeaders = new HttpHeaders(buildContentLengthHeader(LENGTH),
+                buildDateHeader(expectedDate));
+
+        final int statusCode = 200;
+        stubUrl(TEST_PATH, expectedHeaders, statusCode);
+
+        Main.main(expectedUrl);
+
+        assertErrorDocument(expectedUrl, expectedError);
     }
 
     @Test
-    public void errorReport_WhenResponseRandom() {
+    public void errorDocument_WhenProtocolNotSupported() {
 
-        Main.main("foo/bar");
+        final String expectedUrl = "ftp://dr@who/test";
+        final String expectedError = "Protocol not supported";
+
+        Main.main(expectedUrl);
+
+        assertErrorDocument(expectedUrl, expectedError);
     }
 
     @Test
-    public void errorReport_WhenRequestTimesOut() {
+    public void errorDocument_WhenProtocolMissing() {
 
-        Main.main("foo/bar");
+        final String expectedUrl = "www.bbc.co.uk";
+        final String expectedError = "Protocol is required";
+
+        Main.main(expectedUrl);
+
+        assertErrorDocument(expectedUrl, expectedError);
     }
 
     @Test
-    public void errorReport_WhenConnectionReset() {
+    public void errorDocument_WhenResponseMalformed() {
 
-        Main.main("foo/bar");
+        startMockServer(MOCK_SERVER);
+
+        final String expectedUrl = buildExpectedHttpUrl(MOCK_SERVER, TEST_PATH);
+        final String expectedError = "Response is Malformed";
+
+        wireMockServer.stubFor(get(urlEqualTo(expectedUrl))
+                .willReturn(aResponse()
+                        .withFault(Fault.MALFORMED_RESPONSE_CHUNK)));
+
+        Main.main(expectedUrl);
+
+        assertErrorDocument(expectedUrl, expectedError);
     }
 
     @Test
-    public void errorReport_WhenResponseEmpty() {
+    public void errorDocument_WhenResponseRandom() {
 
-        Main.main("foo/bar");
+        startMockServer(MOCK_SERVER);
+
+        final String expectedUrl = buildExpectedHttpUrl(MOCK_SERVER, TEST_PATH);
+        final String expectedError = "Response is Malformed";
+
+        wireMockServer.stubFor(get(urlEqualTo(expectedUrl))
+                .willReturn(aResponse()
+                        .withFault(Fault.RANDOM_DATA_THEN_CLOSE)));
+
+        Main.main(expectedUrl);
+
+        assertErrorDocument(expectedUrl, expectedError);
+    }
+
+    @Test
+    public void errorDocument_WhenRequestTimesOut() {
+
+        startMockServer(MOCK_SERVER);
+
+        final String expectedUrl = buildExpectedHttpUrl(MOCK_SERVER, TEST_PATH);
+        final String expectedError = "Request Timed Out";
+
+        wireMockServer.stubFor(get(urlEqualTo(expectedUrl))
+                .willReturn(aResponse()
+                        .withFixedDelay(10000)));
+
+        Main.main(expectedUrl);
+
+        assertErrorDocument(expectedUrl, expectedError);
+    }
+
+    @Test
+    public void errorDocument_WhenConnectionReset() {
+
+        startMockServer(MOCK_SERVER);
+
+        final String expectedUrl = buildExpectedHttpUrl(MOCK_SERVER, TEST_PATH);
+        final String expectedError = "Connection reset";
+
+        wireMockServer.stubFor(get(urlEqualTo(expectedUrl))
+                .willReturn(aResponse()
+                        .withFault(Fault.CONNECTION_RESET_BY_PEER)));
+
+        Main.main(expectedUrl);
+
+        assertErrorDocument(expectedUrl, expectedError);
+    }
+
+    @Test
+    public void errorDocument_WhenResponseEmpty() {
+
+        startMockServer(MOCK_SERVER);
+
+        final String expectedUrl = buildExpectedHttpUrl(MOCK_SERVER, TEST_PATH);
+        final String expectedError = "Connection reset";
+
+        wireMockServer.stubFor(get(urlEqualTo(expectedUrl))
+                .willReturn(aResponse()
+                        .withFault(Fault.EMPTY_RESPONSE)));
+
+        Main.main(expectedUrl);
+
+        assertErrorDocument(expectedUrl, expectedError);
+    }
+
+    private void startMockServer(String address) {
+
+        wireMockServer = new WireMockServer(options().bindAddress(address).dynamicPort().dynamicHttpsPort());
+        wireMockServer.start();
+    }
+
+    // Conforms to the standard defined in RFC7231 for date formatting https://tools.ietf.org/html/rfc7231#section-7.1.1.1
+    private String getDate() {
+
+        return DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now());
+    }
+
+    private String buildExpectedHttpsUrl(String serverAddress, String path) {
+
+        return "https://" + serverAddress + ":" + wireMockServer.httpsPort() + TEST_PATH;
+    }
+
+    private String buildExpectedHttpUrl(String serverAddress, String path) {
+
+        return "http://" + serverAddress + ":" + wireMockServer.port() + TEST_PATH;
     }
 
     private HttpHeader buildContentLengthHeader(long length) {
@@ -153,10 +360,50 @@ public class MainTest {
         return new HttpHeader(CONTENT_LENGTH, Long.toString(length));
     }
 
-    // Conforms to the standard defined in RFC7231 for date formatting https://tools.ietf.org/html/rfc7231#section-7.1.1.1
-    private HttpHeader buildDateHeader(ZonedDateTime date) {
+    private HttpHeader buildDateHeader(String date) {
 
-        return new HttpHeader(CONTENT_LENGTH, DateTimeFormatter.RFC_1123_DATE_TIME.format(date));
+        return new HttpHeader(CONTENT_LENGTH, date);
+    }
+
+    private void stubUrl(String expectedUrl, HttpHeaders expectedHeaders, int statusCode) {
+
+        wireMockServer.stubFor(get(urlEqualTo(expectedUrl))
+                .willReturn(aResponse()
+                        .withStatus(statusCode)
+                        .withHeaders(expectedHeaders)));
+    }
+
+    private String[] extractStandardOutput() {
+
+        final String output = outContent.toString();
+        return output.split(lineEnding);
+    }
+
+    private void assertStatusDocument(String expectedDateFormatted, String expectedUrl, int statusCode,
+                                      Long expectedLength, String[] outputLines) {
+
+        assertThat(outputLines)
+                .hasSize(6)
+                .containsExactly(
+                        "{",
+                        "\"Url\": \"" + expectedUrl + "\",",
+                        "\"Status_code\": " + statusCode + ",",
+                        "\"Content_length\": " + expectedLength + ",",
+                        "\"Date\": \"" + expectedDateFormatted + "\"",
+                        "}"
+                );
+    }
+
+    private void assertErrorDocument(String expectedUrl, String expectedError) {
+
+        assertThat(extractStandardOutput())
+                .hasSize(4)
+                .containsExactly(
+                        "{",
+                        "\"Url\": \"" + expectedUrl + "\",",
+                        "\"Error\": \"" + expectedError + "\",",
+                        "}"
+                );
     }
 
 }
