f7cd5be397b29988a4549935e20096c6e9ece9db
Neil Bateman <neil.bateman@tide.co>
2017-11-04 10:54:36 +0000

    Powermock Url and UrlConnection

--- src/test/java/org/neil/main/url/UrlTesterTest.java
+++ src/test/java/org/neil/main/url/UrlTesterTest.java
@@ -1,7 +1,5 @@
 package org.neil.main.url;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.http.HttpHeaders;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +8,7 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import javax.net.ssl.HttpsURLConnection;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -21,7 +20,6 @@ import static java.util.Collections.singletonList;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
-import static org.neil.main.utils.HttpTestUtils.*;
 
 /**
  * Tests for the UrlTester implementations and building reports.
@@ -29,8 +27,6 @@ import static org.neil.main.utils.HttpTestUtils.*;
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({URL.class, URLConnection.class, GetRequest.class})
 public class UrlTesterTest {
-
-    private WireMockServer wireMockServer;
 
     @Mock
     private UrlBuilder urlBuilder;
@@ -55,12 +51,34 @@ public class UrlTesterTest {
         mockHeaders.put("Date", Collections.singletonList("date"));
 
         when(urlBuilder.verify(url)).thenReturn(true);
+        when(urlBuilder.build(url)).thenReturn(mockUrl);
         PowerMockito.when(mockUrl.getProtocol()).thenReturn("http");
         PowerMockito.when(mockUrl.openConnection()).thenReturn(mockHttpConnection);
         PowerMockito.when(mockHttpConnection.getResponseCode()).thenReturn(200);
         PowerMockito.when(mockHttpConnection.getHeaderFields()).thenReturn(mockHeaders);
 
+        final List<UrlReport> urlReports = urlTester.test(singletonList(url), urlBuilder);
+
+        assertSoftly(softly -> softly.assertThat(urlReports).containsExactly(expectedReport));
+    }
+
+    @Test
+    public void urlReport_WhenHttpsRequestComplete() throws Exception {
+
+        final String url = "https://www.bbc.co.uk";
+        final UrlTestReport expectedReport = new UrlTestReport(url, 200, 12345L, "date");
+        final URL mockUrl = PowerMockito.mock(URL.class);
+        final HttpURLConnection mockHttpConnection = PowerMockito.mock(HttpsURLConnection.class);
+        final Map<String, List<String>> mockHeaders = new HashMap<>();
+        mockHeaders.put("Content-Length", Collections.singletonList("12345"));
+        mockHeaders.put("Date", Collections.singletonList("date"));
+
+        when(urlBuilder.verify(url)).thenReturn(true);
         when(urlBuilder.build(url)).thenReturn(mockUrl);
+        PowerMockito.when(mockUrl.getProtocol()).thenReturn("https");
+        PowerMockito.when(mockUrl.openConnection()).thenReturn(mockHttpConnection);
+        PowerMockito.when(mockHttpConnection.getResponseCode()).thenReturn(200);
+        PowerMockito.when(mockHttpConnection.getHeaderFields()).thenReturn(mockHeaders);
 
         final List<UrlReport> urlReports = urlTester.test(singletonList(url), urlBuilder);
 
@@ -73,18 +91,13 @@ public class UrlTesterTest {
         final String url = "http://bogus.co";
         final UrlErrorReport expectedReport = new UrlErrorReport(url, "Url could not be connected to");
         when(urlBuilder.verify(url)).thenReturn(true);
-        when(urlBuilder.build(url)).thenReturn(new URL(url));
 
         final URL mockUrl = PowerMockito.mock(URL.class);
-        final HttpURLConnection mockHttpConnection = PowerMockito.mock(HttpURLConnection.class);
-        final Map<String, List<String>> mockHeaders = new HashMap<>();
-        mockHeaders.put("Content-Length", Collections.singletonList("12345"));
-        mockHeaders.put("Date", Collections.singletonList("date"));
 
         when(urlBuilder.verify(url)).thenReturn(true);
+        when(urlBuilder.build(url)).thenReturn(mockUrl);
         PowerMockito.when(mockUrl.getProtocol()).thenReturn("http");
         PowerMockito.when(mockUrl.openConnection()).thenThrow(new IOException("I am Error"));
-
         final List<UrlReport> urlReports = urlTester.test(singletonList(url), urlBuilder);
 
         assertSoftly(softly -> softly.assertThat(urlReports).containsExactly(expectedReport));
@@ -124,22 +137,31 @@ public class UrlTesterTest {
         final UrlTestReport expectedReportUrl1 = new UrlTestReport(url1, 200, 12345L, "date");
         final UrlErrorReport expectedReportUrl2 = new UrlErrorReport(url2, "URL Malformed");
         final UrlTestReport expectedReportUrl3 = new UrlTestReport(url3, 200, 12345L, "date");
+        final URL mockUrl1 = PowerMockito.mock(URL.class);
+        final URL mockUrl3 = PowerMockito.mock(URL.class);
+        final HttpURLConnection mockHttpConnection = PowerMockito.mock(HttpURLConnection.class);
+        final HttpURLConnection mockHttpsConnection = PowerMockito.mock(HttpsURLConnection.class);
+        final Map<String, List<String>> mockHeaders = new HashMap<>();
+        mockHeaders.put("Content-Length", Collections.singletonList("12345"));
+        mockHeaders.put("Date", Collections.singletonList("date"));
+
         when(urlBuilder.verify(url1)).thenReturn(true);
-        when(urlBuilder.build(url1)).thenReturn(new URL(url1));
+        when(urlBuilder.build(url1)).thenReturn(mockUrl1);
+        PowerMockito.when(mockUrl1.getProtocol()).thenReturn("http");
+        PowerMockito.when(mockUrl1.openConnection()).thenReturn(mockHttpConnection);
+        PowerMockito.when(mockHttpConnection.getResponseCode()).thenReturn(200);
+        PowerMockito.when(mockHttpConnection.getHeaderFields()).thenReturn(mockHeaders);
+
         when(urlBuilder.verify(url2)).thenReturn(false);
         when(urlBuilder.verify(url3)).thenReturn(true);
-        when(urlBuilder.build(url3)).thenReturn(new URL(url3));
-        startMockServer(url1);
+        when(urlBuilder.build(url3)).thenReturn(mockUrl3);
 
-        stubUrl(wireMockServer,
-                url1,
-                new HttpHeaders(Arrays.asList(buildDateHeader("date"), buildContentLengthHeader(12345L))),
-                200);
-
-        stubUrl(wireMockServer,
-                url3,
-                new HttpHeaders(Arrays.asList(buildDateHeader("date"), buildContentLengthHeader(12345L))),
-                200);
+        when(urlBuilder.verify(url3)).thenReturn(true);
+        when(urlBuilder.build(url3)).thenReturn(mockUrl3);
+        PowerMockito.when(mockUrl3.getProtocol()).thenReturn("https");
+        PowerMockito.when(mockUrl3.openConnection()).thenReturn(mockHttpsConnection);
+        PowerMockito.when(mockHttpsConnection.getResponseCode()).thenReturn(200);
+        PowerMockito.when(mockHttpsConnection.getHeaderFields()).thenReturn(mockHeaders);
 
         final List<UrlReport> urlReports = urlTester.test(Arrays.asList(url1, url2, url3), urlBuilder);
 
