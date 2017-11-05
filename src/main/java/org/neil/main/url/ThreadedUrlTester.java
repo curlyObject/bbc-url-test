eee7a63bc9c5ce09242a5ab40adb90c4c3e99651
Neil Bateman <neil.bateman@tide.co>
2017-11-05 15:52:34 +0000

    Documentation For url tester

--- src/main/java/org/neil/main/url/ThreadedUrlTester.java
+++ src/main/java/org/neil/main/url/ThreadedUrlTester.java
@@ -17,7 +17,7 @@ public class ThreadedUrlTester implements UrlTester {
      * Converts the URL string into a URL object and then performs the GET request.
      * Will collect response information and collate into UrlReport objects.
      * Uses the parallelStream method to multi thread the calls, minimising the effect a slow request or non
-     * respondant endpoint has.
+     * respondent endpoint has.
      *
      * @param urls       The list of url strings to perform GET requests on.
      * @param urlBuilder The UrlBuilder object instance to use for url string verification and conversion.
@@ -41,6 +41,13 @@ public class ThreadedUrlTester implements UrlTester {
 
     }
 
+    /**
+     * Convert a Http Response into a Url report
+     *
+     * @param urlString    The url that this report is about
+     * @param httpResponse The Http response collected for this url
+     * @return A UrlReport of the respsonse
+     */
     private UrlReport buildUrlTestReport(String urlString, HttpResponse httpResponse) {
 
         final Long contentLength = httpResponse.geContentLengthHeader().map(Long::valueOf).orElse(null);
@@ -51,6 +58,11 @@ public class ThreadedUrlTester implements UrlTester {
                 httpResponse.getDateHeader().orElse(null));
     }
 
+    /**
+     * Attempts to connect and retrieve response for the provided URL
+     * @param url The url to connect to
+     * @return An Optional of the Http responses if it could dbe connected ot or any empty optional if the connection failed.
+     */
     private Optional<HttpResponse> getHttpResponse(URL url) {
 
         try {
@@ -60,6 +72,12 @@ public class ThreadedUrlTester implements UrlTester {
         }
     }
 
+    /**
+     * Verifies and converts a string representation of a url to a URL object.
+     * @param url The string representing a url to verify and build
+     * @param urlBuilder The object to use to build and verify the url.
+     * @return An optional of the URL object for the provided String if it is a valid url, otherwise returns an empty Optional
+     */
     private Optional<URL> buildUrl(String url, UrlBuilder urlBuilder) {
 
         if (urlBuilder.verify(url)) {
